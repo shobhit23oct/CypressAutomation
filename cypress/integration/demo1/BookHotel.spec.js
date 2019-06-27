1f75709c52a00ca15019b88fc473deec97c908f4
Shobhit Anand <shobhit23oct@gmail.com>
2019-06-28 01:29:38 +0530

    Comments added

--- cypress/integration/demo1/BookHotel.spec.js
+++ cypress/integration/demo1/BookHotel.spec.js
@@ -1,27 +1,32 @@
 describe('DEMO', function() {
   it('Login', function() {
+
+//Launching the site
     cy.visit('https://www.cleartrip.com/')
 
+//Handling Exception
     cy.on('uncaught:exception', (err, runnable) => {
       return false;
     });
+
+//Searching for the place to book the hotel
     cy.get('.withArrows > .hotelApp > [href="/hotels"]').click()
     cy.get('#Tags').type('Jayanagar, Bangalore', {force: true})
     .should('have.value', 'Jayanagar, Bangalore');
-
     cy.wait(5000);
-
     cy.get('#Tags').type('{enter}');
 
+//Selecting the CheckInDate
 const inDate = Cypress.moment().add(3, 'days').format('ddd, MMM D YYYY');
 cy.get('#CheckInDate').type(`${inDate}`, { force: true });
-
 cy.wait(2000)
 
+//Selecting the CheckOutDate
 const outDate = Cypress.moment().add(5, 'days').format('ddd, MMM D YYYY');
-     cy.get('#CheckOutDate').type(`${outDate}`, { force: true });
+cy.get('#CheckOutDate').type(`${outDate}`, { force: true });
 cy.wait(2000);
 
+//Searching for the hotel
 cy.get('#SearchHotelsButton').click({ force: true });
 cy.wait(5000)
 cy.get('.clearFix.flex > .right-content-section > .tools > .fRight > .booking').click()
--- cypress/integration/demo2/BookFlight.spec.js
+++ cypress/integration/demo2/BookFlight.spec.js
@@ -1,30 +1,39 @@
 describe('DEMO', function() {
   it('Login', function() {
-    cy.visit('https://www.cleartrip.com/')
 
-    cy.on('uncaught:exception', (err, runnable) => {
-      return false;
-    });
+//Launching the site
+  cy.visit('https://www.cleartrip.com/')
+
+//Handling the Exception
+  cy.on('uncaught:exception', (err, runnable) => {
+  return false;
+  });
   cy.wait(5000);
+
+//Searching for the airport to depart
   cy.get('#FromTag').type('Bangalore, IN - Kempegowda International Airport (BLR)', {force: true})
-cy.wait(2000)
-cy.get('#FromTag').type('{enter}');
-cy.get('#ToTag').type('Mumbai, IN - Chatrapati Shivaji Airport (BOM)', {force: true})
-    cy.wait(2000);
+  cy.wait(2000)
+  cy.get('#FromTag').type('{enter}');
+
+//Searching for the airport to arrive
+  cy.get('#ToTag').type('Mumbai, IN - Chatrapati Shivaji Airport (BOM)', {force: true})
+  cy.wait(2000);
   cy.get('#ToTag').type('{enter}');
 
+//Selecting the depart date 15 days after the current date
  const todaysDate = Cypress.moment().add(15, 'days').format('ddd, MMM D YYYY');
+ cy.get('#DepartDate').type(`${todaysDate}`, { force: true });
+ cy.get('#DepartDate').type('{enter}');
 
+//Selecting two adults from the dropdown
+ cy.get('#Adults').select('2').should('have.value','2');
 
-cy.get('#DepartDate').type(`${todaysDate}`, { force: true });
-cy.get('#DepartDate').type('{enter}');
+//Searching for the flight
+ cy.get('#SearchBtn').click()
+ cy.wait(5000)
 
-cy.get('#Adults').select('2').should('have.value','2');
-cy.get('#SearchBtn').click()
-
-cy.wait(5000)
-cy.get('.price > .current').click()
-
+//Sorting the flight as per the price in decreasing order
+ cy.get('.price > .current').click()
  cy.wait(5000)
  cy.get('.showTabs > .resultUnit > .segment > :nth-child(2) > .price > .booking').click()
  cy.wait(5000)
